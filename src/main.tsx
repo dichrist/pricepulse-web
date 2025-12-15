0bc444ac944aa7d2c1b0d3e69230ad64b182f07d
dichrist <diegosouzasilva91@gmail.com>
2025-12-15 02:12:42 -0300

    Main file of site

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LoginPage from "./pages/LoginPage";
+import PriceEntriesPage from "./pages/PriceEntriesPage";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const qc = new QueryClient();
+
+const router = createBrowserRouter([
+  { path: "/", element: <LoginPage /> },
+  { path: "/price-entries", element: <PriceEntriesPage /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={qc}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
